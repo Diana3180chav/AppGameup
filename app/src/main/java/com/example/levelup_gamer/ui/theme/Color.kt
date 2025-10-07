78ebd9ea8f62a1bd0c58e6cd8da29b81ca35d7e9
Diana3180chav <dia.chavez@duocuc.cl>
2025-10-07 05:19:38 -0300

    Login y modificacion de colores

--- app/src/main/java/com/example/levelup_gamer/ui/theme/Color.kt
+++ app/src/main/java/com/example/levelup_gamer/ui/theme/Color.kt
@@ -10,6 +10,11 @@ val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
+val loginBg      = Color(0xFF000000) // Fondo negro puro
+val neonBlue     = Color(0xFF00E5FF) // Azul fluorescente principal
+val neonBlueDim  = Color(0x9900E5FF) // Azul fluorescente con transparencia (bordes no enfocados)
+val textOnDark   = Color(0xFFEFEFEF) // Texto claro sobre fondo negro
+val errorRed     = Color(0xFFFF5252) // Para estados de error (inputs)
 //Colores personalizados
 
 val fondoPrincipal = Color(0xFF000000)
@@ -22,4 +27,5 @@ val textosecundario = Color(0xFFD3D3D3)
 val registerFondo = Color(0x7021063F)
 val registerTitle = Color(0xFF000000)
 val registerlabel = Color(0xFFD3D3D3)
+
 
