500b7bab9b42cc6927b789c6e9baf6c8dd8f91cf
angelo-stephano-1993 <ang.galindo@duocuc.cl>
2025-11-29 09:40:58 -0300

    se agrega /auth/ a retrofit para que se conecte con la ruta del back

--- app/src/main/java/com/example/levelup_gamer/repository/api/RetrofirInstance.kt
+++ app/src/main/java/com/example/levelup_gamer/repository/api/RetrofirInstance.kt
@@ -5,7 +5,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitInstance {
 
-    private const val BASE_URL = "http://10.0.2.2:8080/api/"
+    private const val BASE_URL = "http://10.0.2.2:8080/api/auth"
 
     val api: ApiService by lazy{
         Retrofit.Builder()
